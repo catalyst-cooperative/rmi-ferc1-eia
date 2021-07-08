f6068707aadc2084f71add2d9a48b865b01872d4
cbz <cgosnell@catalyst.coop>
2021-07-08 14:55:45 -0400

    omigosh do the cleanstrings_snake in the right place

--- src/pudl_rmi/connect_ferc1_to_eia.py
+++ src/pudl_rmi/connect_ferc1_to_eia.py
@@ -163,6 +163,7 @@ class InputManager:
                 .assign(plant_part=lambda x: x['appro_part_label'],
                         record_id_eia=lambda x: x['appro_record_id_eia'])
                 .pipe(make_plant_parts_eia.reassign_id_ownership_dupes)
+                .pipe(pudl.helpers.cleanstrings_snake, ['record_id_eia'])
                 .replace(to_replace="nan", value={'record_id_eia': pd.NA, })
                 # recordlinkage and sklearn wants MultiIndexs to do the stuff
                 .set_index(['record_id_ferc1', 'record_id_eia', ])
