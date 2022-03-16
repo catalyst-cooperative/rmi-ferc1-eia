651e6061bdbe2a3a0ab5621a8cd8f7fdfa906870
cbz <cgosnell@catalyst.coop>
2022-03-15 20:00:52 -0400

    black-i-fy and beef up documentation

--- test/integration/rmi_out_test.py
+++ test/integration/rmi_out_test.py
@@ -5,7 +5,8 @@ This can take up to an hour to run.
 """
 
 import logging
-from typing import Literal
+from typing import Literal, List
+import numpy as np
 
 import pandas as pd
 import pytest
@@ -16,19 +17,20 @@ import pudl_rmi
 logger = logging.getLogger(__name__)
 
 
-PK_UTILS = ['report_year', 'data_source', 'utility_id_pudl']
-PK_PLANTS = ['report_year', 'data_source', 'utility_id_pudl', 'plant_id_eia']
-PATH_FAIL = pudl_rmi.INPUTS_DIR / 'validation'
+PK_UTILS = ["report_year", "data_source", "utility_id_pudl"]
+PK_PLANTS = ["report_year", "data_source", "utility_id_pudl", "plant_id_eia"]
+PATH_FAIL = pudl_rmi.INPUTS_DIR / "validation"
 
 
 @pytest.mark.parametrize(
-    "table_name", [
+    "table_name",
+    [
         "fuel_ferc1",
         "ownership_eia860",
         "plants_entity_eia",
         "fuel_receipts_costs_eia923",
         "utilities_pudl",
-    ]
+    ],
 )
 def test_pudl_engine(pudl_engine, table_name):
     """Test that the PUDL DB is actually available."""
@@ -73,6 +75,59 @@ def test_deprish_to_ferc1(rmi_out):
     assert not deprish_to_ferc1.empty
 
 
+##################
+# Validation Tests
+##################
+
+
+def gb_test(
+    df1: pd.DataFrame, df2: pd.DataFrame, data_col: str, by: List[str]
+) -> pd.DataFrame:
+    """
+    Merge two grouped input tables to determine if summed data column matches.
+
+    TODO: Please help me name this....
+
+    Args:
+        df1: One dataframe to sum and check consistency with ``df2``.
+        df2: Other dataframe to sum and check consistency against ``df1``.
+        data_col: data column to check. Column must be in both ``df1`` and
+            ``df2``.
+    """
+    return (
+        pd.merge(
+            group_sum_col(df1, data_col=data_col, by=by),
+            group_sum_col(df2, data_col=data_col, by=by),
+            right_index=True,
+            left_index=True,
+            suffixes=("_1", "_2"),
+            how="outer",
+        )
+        .assign(
+            match=lambda x: np.isclose(
+                x[f"{data_col}_1"], x[f"{data_col}_2"], equal_nan=True
+            ),
+            off_rate=lambda x: x[f"{data_col}_1"] / x[f"{data_col}_2"],
+        )
+        .sort_index()
+    )
+
+
+def group_sum_col(df, data_col: str, by: List[str]) -> pd.DataFrame:
+    """Groupby sum a specific table's data col."""
+    gb_out = (
+        df.assign(  # convert date to year bc many of the og depish studies are EOY
+            report_year=lambda x: x.report_date.dt.year
+        )
+        .astype({"report_year": pd.Int64Dtype()})[
+            df.plant_id_eia.notnull()
+        ]  # only plant associated reocrds
+        .groupby(by=by, dropna=True)[[data_col]]
+        .sum(min_count=1)
+    )
+    return gb_out
+
+
 def _add_data_source(df):
     """
     Add a data_source column with 'FERC' and 'PUC'.
@@ -84,113 +139,139 @@ def _add_data_source(df):
     the FERC-EIA-Depreciation tables we need to add *both* versions of the
     depreciation `data_source`.
     """
-    if 'data_source' not in df:
-        df = pd.concat([
-            df.assign(data_source='PUC'),
-            df.assign(data_source='FERC'),
-        ])
+    if "data_source" not in df:
+        df = pd.concat(
+            [
+                df.assign(data_source="PUC"),
+                df.assign(data_source="FERC"),
+            ]
+        )
     return df
 
 
 @pytest.mark.parametrize(
-    "df1_name,df2_name,data_col,by_name", [
+    "df1_name,df2_name,data_col,by_name",
+    [
+        ("grab_deprish", "grab_deprish_to_eia", "plant_balance_w_common", "utilities"),
         (
             "grab_deprish",
             "grab_deprish_to_eia",
-            'plant_balance_w_common',
-            'utilities'
-        ),
-        (
-            "grab_deprish",
-            "grab_deprish_to_eia",
-            'plant_balance_w_common',
-            'plants',
+            "plant_balance_w_common",
+            "plants",
         ),
         (
             "grab_deprish_to_eia",
             "grab_deprish_to_ferc1",
-            'plant_balance_w_common',
-            'utilities',
+            "plant_balance_w_common",
+            "utilities",
         ),
         (
             "grab_deprish_to_eia",
             "grab_deprish_to_ferc1",
-            'plant_balance_w_common',
-            'plants',
+            "plant_balance_w_common",
+            "plants",
         ),
         (
             "grab_deprish",
             "grab_deprish_to_ferc1",
-            'plant_balance_w_common',
-            'utilities'
+            "plant_balance_w_common",
+            "utilities",
         ),
         (
             "grab_deprish",
             "grab_deprish_to_ferc1",
-            'plant_balance_w_common',
-            'plants',
+            "plant_balance_w_common",
+            "plants",
         ),
         (
             "grab_ferc1_to_eia",
             "grab_deprish_to_ferc1",
-            'capex_total',
-            'utilities',
+            "capex_total",
+            "utilities",
         ),
         (
             "grab_ferc1_to_eia",
             "grab_deprish_to_ferc1",
-            'capex_total',
-            'plants',
+            "capex_total",
+            "plants",
         ),
-    ]
+    ],
 )
 def test_consistency_of_data_stages(
-    rmi_out,
+    rmi_out: pudl_rmi.coordinate.Output,
     df1_name: str,
     df2_name: str,
     data_col: str,
-    by_name: Literal['plants', 'utilities'],
+    by_name: Literal["plants", "utilities"],
 ):
     """
     Test the consistency of a data column at two stages.
 
     The data that is processed in this repo goes along multiple stages of its
     journey. There are some quantities that are expected to be invariant when
-    aggregated at the plant level, and the utility level -- that the quantity
-    might slosh around between generators in a plant, or between plants within
-    a utility, but that when aggregated to the plant or utility level, it
-    should be the same at every step in the processing.
+    aggregated at the plant level, and the utility level -- that quantity might
+    slosh around between generators in a plant, or between plants within a
+    utility, but that when aggregated to the plant or utility level, it should
+    be the same at every step in the processing.
 
-    Ideally, the inputs
+    This test enables iteration over any two stages with a specific ``data_col``
+    to test by aggregating both stages' df by either `plants` or `utilities` and
+    checking if those plant or utility sums are exactly the same in each of the
+    two stages.
+
+    There are a relatively small set of known cases of inconsistencies between
+    these stages. We have saved these known inconsistencies and hope to decrease
+    them over time. When a code change causes either more errors or unexpected
+    fixes to the consistency of these stages this test will fail.
+
+    Nulls and connections... There are a lot of depreciation and FERC1 records
+    that are not connected to the EIA plant-parts. These un-connected records
+    are often not able to be propagated through the rest of the processing
+    stages. Because of this, we often have more data in early stages than we do
+    in later stages. This test is only testing the records which have non-null
+    values in the aggregated ``data_col`` for both stages. We are avoiding the
+    nulls because there are many of them and they would drown out the truly
+    wrong/inconsistent data. (In truth, it is probable that many of the known
+    inconsistencies are also caused by missing connections to EIA.)
+
+    Note: this will not be effective on any of the rate columns like
+    `depreciation_annual_rate` or `avg_num_employees`. The columns which this
+    would work on are roughly synonymous with the columns which recieve a
+    `scale` treatement type in :mod:``pudl_rmi.connect_deprish_to_ferc1.py``
     """
-    if by_name == 'plants':
+    if by_name == "plants":
         by = PK_PLANTS
-    elif by_name == 'utilities':
+    elif by_name == "utilities":
         by = PK_UTILS
+    else:
+        raise AssertionError(
+            "This test only takes `plants` or `utilities` as an argument for `by_name`"
+        )
 
-    test_stages = pudl_rmi.connect_deprish_to_ferc1.gb_test(
+    test_stages = gb_test(
         df1=rmi_out.__getattribute__(df1_name)().pipe(_add_data_source),
         df2=rmi_out.__getattribute__(df2_name)().pipe(_add_data_source),
         data_col=data_col,
-        by=by
-
+        by=by,
     )
-    # we add the off_rate.notnll() mask in here bc there are a ton of unconnected
-    # data in these tables (eg. depreciation records that don't have a EIA connection)
-    # these bbs will not get effectively propegated through each of these stages that
-    # require a match to EIA.
-    # there are enough of them that we want to generally ignore them
+    # there are a ton of unconnected data in these tables (eg. depreciation
+    # records that don't have a EIA connection) these records will not get
+    # effectively propegated through each of these stages that require a match
+    # to EIA. the `off_rate` column being null is an indication that either df1
+    # or df2 has a null data_col.
+    # there are enough nulls in one of the aggregated ``data_col`` that we want
+    # to generally ignore these records
     fails = test_stages[~test_stages.match & test_stages.off_rate.notnull()]
 
+    df1_name_simple = df1_name.replace("grab_", "")
+    df2_name_simple = df2_name.replace("grab_", "")
     logger.info(
-        f"Failures for {data_col} by {by_name} btwn {df1_name.replace('grab_', '')} "
-        f"and {df2_name.replace('grab_', '')}: {len(fails)}"
+        f"Failures for {data_col} by {by_name} btwn {df1_name_simple} "
+        f"and {df2_name_simple}: {len(fails)}"
     )
-
     fail_path = (
-        PATH_FAIL /
-        f"fail_{data_col}_{by_name}_{df1_name.replace('grab_', '')}_vs_"
-        f"{df2_name.replace('grab_', '')}.csv"
+        PATH_FAIL / f"fail_{data_col}_{by_name}_{df1_name_simple}_vs_"
+        f"{df2_name_simple}.csv"
     )
     fails_expected = pd.read_csv(fail_path).set_index(by)
     pd.testing.assert_index_equal(fails.index, fails_expected.index)
