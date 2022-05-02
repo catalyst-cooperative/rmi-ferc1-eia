6cacf98d49e260dbc7414c97840045a3336b831a
Austen Sharpe <austensharpe@gmail.com>
2022-05-02 16:41:08 -0600

    merge deprish on utility_id_eia AND report_year

--- src/pudl_rmi/create_override_spreadsheets.py
+++ src/pudl_rmi/create_override_spreadsheets.py
@@ -234,7 +234,9 @@ def _prep_deprish(deprish, pudl_out) -> pd.DataFrame:
     util_df = pudl_out.utils_eia860()[["utility_id_pudl", "utility_id_eia"]]
     deprish_out = deprish.copy()
     deprish_out.loc[:, "report_year"] = deprish_out.report_date.dt.year.astype("Int64")
-    deprish_out = deprish_out.merge(util_df, on="utility_id_pudl", how="left")
+    deprish_out = deprish_out.merge(
+        util_df, on=["report_year", "utility_id_pudl"], how="left"
+    )
 
     return deprish_out
 
